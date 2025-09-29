43e7105251baf4398ffc8f7d6bb8d0665e85f172
leejinwoo <julee@vessellink.com>
2025-09-29 18:31:48 +0900

    Contexts, Entities 정의, 엔티티 생성

---
+++ ShipParticularsApi/Entities/ShipInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShipParticularsApi.Entities
+{
+    [Table("SHIP_INFO")]
+    [Index(nameof(ShipKey), IsUnique = true)]
+    public class ShipInfo
+    {
+        [Key]
+        [Column("ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        [Column("SHIP_KEY")]
+        [MaxLength(10)]
+        public string ShipKey { get; set; }
+
+        [Column("CALLSIGN")]
+        [MaxLength(10)]
+        public string Callsign { get; set; }
+
+        [Column("SHIP_NAME")]
+        [MaxLength(10)]
+        public string ShipName { get; set; }
+
+        [Column("SHIP_TYPE")]
+        [MaxLength(100)]
+        public string ShipType { get; set; }
+
+        [Column("SHIP_CODE")]
+        [MaxLength(100)]
+        public string ShipCode { get; set; }
+
+        [Column("EXTERNAL_SHIP_ID")]
+        [MaxLength(36)]
+        public string ExternalShipId { get; set; }
+
+        [Column("IS_USE_KTSAT")]
+        public bool IsUseKtsat { get; set; }
+
+        [Column("IS_SERVICE")]
+        public bool IsService { get; set; }
+
+        [Column("IS_USE_AIS")]
+        public bool IsUseAis { get; set; }
+
+        public virtual ICollection<ReplaceShipName>? ReplaceShipNames { get; set; }
+        public virtual ICollection<ShipModelTest>? ShipModelTests { get; set; }
+        public virtual ICollection<ShipSatellite>? ShipSatellites { get; set; }
+        public virtual ICollection<ShipService>? ShipServices { get; set; }
+        public virtual ICollection<SkTelinkCompanyShip>? SkTelinkCompanyShips { get; set; }
+    }
+}
---
+++ ShipParticularsApi/Entities/ShipModelTest.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShipParticularsApi.Entities
+{
+    [Table("SHIP_MODEL_TEST")]
+    public class ShipModelTest
+    {
+        [Key]
+        [Column("ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Column("SHIP_KEY")]
+        [Required]
+        [MaxLength(10)]
+        public string ShipKey { get; set; }
+
+        public ShipInfo ShipInfo { get; set; }
+
+        [Column("ZA_BALLAST")]
+        public double ZaBallast { get; set; }
+
+        [Column("TRANSVERSE_PROJECTION_AREA_BALLAST")]
+        public double TransverseProjectionAreaBallast { get; set; }
+
+        [Column("TRANSVERSE_PROJECTION_AREA_SCANTLIN")]
+        public double TransverseProjectionAreaScantling { get; set; }
+
+        [Column("KYY")]
+        public double Kyy { get; set; }
+
+        [Column("DRAFT_FORE")]
+        public double DraftFore { get; set; }
+
+        [Column("DRAFT_AFT")]
+        public double DraftAft { get; set; }
+
+        [Column("CB_BALLAST")]
+        public double CbBallast { get; set; }
+
+        [Column("CB_SCANTLING")]
+        public double CbScantling { get; set; }
+
+        [Column("SUBMERGED_SURFACE_BALLAST")]
+        public double SubmergedSurfaceBallast { get; set; }
+
+        [Column("SUBMERGED_SURFACE_SCANTLING")]
+        public double SubmergedSurfaceScantling { get; set; }
+
+        [Column("MID_SHIP_SECTION_AREA_BALLAST")]
+        public double MidShipSectionAreaBallast { get; set; }
+
+        [Column("MID_SHIP_SECTION_AREA_SCANTLING")]
+        public double MidShipSectionAreaScantling { get; set; }
+
+        [Column("DISPLACEMENT_BALLAST")]
+        public double DisplacementBallast { get; set; }
+
+        [Column("DISPLACEMENT_SCANTLING")]
+        public double DisplacementScantling { get; set; }
+
+        [Column("SPEED_etaD_BALLAST")]
+        public string SpeedEtaDBallast { get; set; }
+
+        [Column("etaD_BALLAST")]
+        public string EtaDBallast { get; set; }
+
+        [Column("SPEED_etaD_SCANTLING")]
+        public string SpeedEtaDScantling { get; set; }
+
+        [Column("etaD_SCANTLING")]
+        public string EtaDScantling { get; set; }
+    }
+}
