610d4b183e1393d4a2ac2e87e653c288ac886dc9
이진우 <leejinwoo1126@gmail.com>
2025-10-01 18:32:16 +0900

    navigation property 조회 테스트 (즉시로딩, 지연로딩 최적화)

---
+++ ShipParticularsApi/Tests/NavigationPropertyTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using ShipParticularsApi.Contexts;
+using ShipParticularsApi.Entities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ShipParticularsApi.Tests
+{
+    public class NavigationPropertyTests
+    {
+        private readonly SqliteConnection _connection;
+        private readonly DbContextOptions<ShipParticularsContext> _options;
+        private readonly ITestOutputHelper _output;
+
+        // NOTE: beforeEach
+        public NavigationPropertyTests(ITestOutputHelper output)
+        {
+            _output = output;
+
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            _options = new DbContextOptionsBuilder<ShipParticularsContext>()
+                .UseSqlite(_connection)
+                .UseLazyLoadingProxies()
+                .LogTo(message => _output.WriteLine(message), LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .Options;
+
+            var context = new ShipParticularsContext(_options);
+            context.Database.EnsureCreated();
+        }
+
+        // NOTE: AfterEach
+        public void Dispose() => _connection.Dispose();
+
+        ShipParticularsContext CreateContext() => new(_options);
+
+        [Fact]
+        public async Task Test2()
+        {
+            // Arrange
+            await using (var arrangeContext = CreateContext())
+            {
+                List<ShipService> shipServices = [];
+                shipServices.Add(new ShipService { ShipKey = "SHIP01", ServiceName = "cctv", IsCompleted = true });
+                shipServices.Add(new ShipService { ShipKey = "SHIP01", ServiceName = "eu-mrv", IsCompleted = true });
+                shipServices.Add(new ShipService { ShipKey = "SHIP01", ServiceName = "noon-report", IsCompleted = false });
+
+                var shipInfo = new ShipInfo
+                {
+                    ShipKey = "SHIP01",
+                    ShipName = "New Vessel",
+                    Callsign = "CALL01",
+                    ReplaceShipName = new ReplaceShipName { ShipKey = "SHIP01", ReplacedShipName = "Next Vessel" },
+                    ShipServices = shipServices
+                };
+
+                arrangeContext.ShipInfos.Add(shipInfo);
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            // Act, Assert
+            await using (var assertContext = CreateContext())
+            {
+                var retrevedShipInfo = await assertContext.ShipInfos
+                    .Include(s => s.ReplaceShipName)
+                    .Include(s => s.ShipServices)
+                    .AsSplitQuery()
+                    .SingleAsync(s => s.ShipKey == "SHIP01");
+                var services = retrevedShipInfo.ShipServices;
+                var replaceShipName = retrevedShipInfo.ReplaceShipName;
+
+                Assert.NotNull(services);
+                Assert.NotNull(replaceShipName);
+            }
+        }
+    }
+}
