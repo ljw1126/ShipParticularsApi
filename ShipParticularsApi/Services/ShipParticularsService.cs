9037d98fbd9b7b24df27b26dee52e66730c907aa
leejinwoo <julee@vessellink.com>
2025-10-02 16:07:14 +0900

    서비스, 인터페이스 생성

---
+++ ShipParticularsApi/Services/ShipParticularsService.cs
@@ -0,0 +1,31 @@
+﻿using ShipParticularsApi.Contexts;
+
+namespace ShipParticularsApi.Services
+{
+    public class ShipParticularsService(ShipParticularsContext dbContext,
+        IReplaceShipNameRepository replaceShipNameRepository,
+        IShipInfoRepository shipInfoRepository,
+        IShipModelTestRepository shipModelTestRepository,
+        IShipSatelliteRepository shipSatelliteRepository,
+        IShipServiceRepository shipServiceRepository,
+        ISkTelinkCompanyShipRepository skTelinkCompanyShipRepository)
+    {
+        public async Task Process(Object param)
+        {
+            await using var transaction = await dbContext.Database.BeginTransactionAsync();
+
+            try
+            {
+                // 비즈니스 로직 작성
+
+                await dbContext.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
