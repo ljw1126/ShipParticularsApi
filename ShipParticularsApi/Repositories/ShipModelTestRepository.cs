d5e1acc13aca63ea5704971a299ac550033b0c4d
leejinwoo <julee@vessellink.com>
2025-10-02 16:07:23 +0900

    Repository 구현체 생성

---
+++ ShipParticularsApi/Repositories/ShipModelTestRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShipParticularsApi.Contexts;
+using ShipParticularsApi.Services;
+
+namespace ShipParticularsApi.Repositories
+{
+    public class ShipModelTestRepository(ShipParticularsContext context) : IShipModelTestRepository
+    {
+    }
+}
---
+++ ShipParticularsApi/Repositories/ShipServiceRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShipParticularsApi.Contexts;
+using ShipParticularsApi.Services;
+
+namespace ShipParticularsApi.Repositories
+{
+    public class ShipServiceRepository(ShipParticularsContext context) : IShipServiceRepository
+    {
+    }
+}
