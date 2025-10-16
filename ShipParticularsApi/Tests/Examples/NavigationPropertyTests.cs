a8a5c907d49d8bfaedc385dbc94edb552a794a31
leejinwoo <julee@vessellink.com>
2025-10-16 14:15:44 +0900

    엔티티 연관관계 null 참조 변경시 DELETE 쿼리 날아가는지 확인하기 위한 테스트 추가

--- ShipParticularsApi/Tests/Examples/NavigationPropertyTests.cs
+++ ShipParticularsApi/Tests/Examples/NavigationPropertyTests.cs
@@ -5,11 +5,11 @@ using ShipParticularsApi.Contexts;
 using ShipParticularsApi.Entities;
 using Xunit;
 using Xunit.Abstractions;
-
 using static ShipParticularsApi.Tests.Builders.ReplaceShipNameTestBuilder;
 using static ShipParticularsApi.Tests.Builders.ShipInfoTestBuilder;
+using static ShipParticularsApi.Tests.Builders.ShipSatelliteTestBuilder;
 using static ShipParticularsApi.Tests.Builders.ShipServiceTestBuilder;
-
+using static ShipParticularsApi.Tests.Builders.SkTelinkCompanyShipTestBuilder;
 
 namespace ShipParticularsApi.Tests.Examples
 {
@@ -76,6 +76,95 @@ namespace ShipParticularsApi.Tests.Examples
                 shipServices.Should().NotBeNullOrEmpty()
                         .And.HaveCount(3);
                 replaceShipName.Should().NotBeNull();
+            }
+        }
+
+        [Fact(DisplayName = "AIS 서비스 사용중 AIS 비활성화하면, 서비스 컬렉션에서 SatAis만 제거된다.")]
+        public async Task Ais_toggle_off_test()
+        {
+            await using (var arrangeContext = CreateContext())
+            {
+                arrangeContext.ShipInfos.Add(ShipInfo()
+                    .WithShipKey("TEST_SHIP_KEY")
+                    .WithShipServices(
+                        ShipService().WithServiceName(ServiceNameTypes.SatAis).WithIsCompleted(true),
+                        ShipService().WithServiceName(ServiceNameTypes.KtSat).WithIsCompleted(true)
+                    )
+                    .Build());
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            await using (var actContext = CreateContext())
+            {
+                ShipInfo target = await actContext.ShipInfos
+                    .Include(s => s.ShipServices)
+                    .SingleAsync(s => s.ShipKey == "TEST_SHIP_KEY");
+
+                target.ManageAisService(false);
+
+                await actContext.SaveChangesAsync();
+            }
+
+            await using (var assertContext = CreateContext())
+            {
+                ShipInfo target = await assertContext.ShipInfos
+                    .Include(s => s.ShipServices)
+                    .SingleAsync(s => s.ShipKey == "TEST_SHIP_KEY");
+
+                target.Should().NotBeNull();
+                target.ShipServices.Should().NotBeEmpty()
+                    .And.HaveCount(1);
+                target.ShipServices.Should().ContainSingle()
+                    .Which.ServiceName.Should().Be(ServiceNameTypes.KtSat);
+            }
+        }
+
+        [Fact(DisplayName = "SK TELINK 위성 사용 중에 GPS 비활성화하면, 모든 관련 자식 엔티티가 DELETE 되고, 부모 컬럼도 업데이트 된다")]
+        public async Task Gps_toggle_off_test()
+        {
+            await using (var arrangeContext = CreateContext())
+            {
+                arrangeContext.ShipInfos.Add(ShipInfo()
+                      .WithShipKey("UNIQUE_SHIP_KEY")
+                      .WithShipServices(KtSatService("UNIQUE_SHIP_KEY"))
+                      .WithShipSatellite(SkTelinkSatellite("UNIQUE_SHIP_KEY", "SATELLITE_ID"))
+                      .WithExternalShipId("SATELLITE_ID")
+                      .WithIsUseKtsat(true)
+                      .WithSkTelinkCompanyShip(SkTelinkCompanyShip("UNIQUE_SHIP_KEY", "UNIQUE_COMPANY_NAME"))
+                      .Build());
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            await using (var actContext = CreateContext())
+            {
+                ShipInfo target = await actContext.ShipInfos
+                    .Include(s => s.ShipServices)
+                    .Include(s => s.ShipSatellite)
+                    .Include(s => s.SkTelinkCompanyShip)
+                    .AsSplitQuery()
+                    .SingleAsync(s => s.ShipKey == "UNIQUE_SHIP_KEY");
+
+                target.ManageGpsService(false, null, null, null);
+
+                await actContext.SaveChangesAsync();
+            }
+
+            await using (var assertContext = CreateContext())
+            {
+                ShipInfo actual = await assertContext.ShipInfos
+                    .Include(s => s.ShipServices)
+                    .Include(s => s.ShipSatellite)
+                    .Include(s => s.SkTelinkCompanyShip)
+                    .AsSplitQuery()
+                    .SingleAsync(s => s.ShipKey == "UNIQUE_SHIP_KEY");
+
+                actual.Should().NotBeNull();
+                actual.ExternalShipId.Should().BeNull();
+                actual.IsUseKtsat.Should().BeFalse();
+
+                actual.ShipServices.Should().BeEmpty();
+                actual.ShipSatellite.Should().BeNull();
+                actual.SkTelinkCompanyShip.Should().BeNull();
             }
         }
     }
