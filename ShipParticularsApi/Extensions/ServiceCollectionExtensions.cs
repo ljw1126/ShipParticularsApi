3afc2a8ddbdcc426092e9fd152d9d866054cdc7c
이진우 <leejinwoo1126@gmail.com>
2025-10-17 11:23:25 +0900

    Extensions

---
+++ ShipParticularsApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ShipParticularsApi.Contexts;
+using ShipParticularsApi.Repositories;
+using ShipParticularsApi.Services;
+
+namespace ShipParticularsApi.Extensions
+{
+    // NOTE. Naming Convention 존재, *Extension으로 클래스명 지으면 Program.cs에서 호출 x
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IShipInfoRepository, ShipInfoRepository>();
+            services.AddScoped<ShipParticularsService>();
+            services.AddScoped<IShipParticularsService>(
+                s => new TransactionDecorator(
+                    s.GetRequiredService<ShipParticularsContext>(),
+                    s.GetRequiredService<ShipParticularsService>()
+                ));
+            return services;
+        }
+    }
+}
