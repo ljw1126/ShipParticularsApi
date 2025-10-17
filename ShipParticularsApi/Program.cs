709b0a6e731fcb05ca8b61619a9548f4bec2b07e
이진우 <leejinwoo1126@gmail.com>
2025-10-17 11:22:43 +0900

    WebApplicationFactory 메서드 오버라이딩, Helper Extensions 추가

--- ShipParticularsApi/Program.cs
+++ ShipParticularsApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ShipParticularsApi.Contexts;
+using ShipParticularsApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ShipParticularsContext>(options
     => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddApplicationServices();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,3 +32,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
---
+++ ShipParticularsApi/Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Common;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using ShipParticularsApi.Contexts;
+
+namespace ShipParticularsApi.Tests
+{
+    // NOTE. 스프링에 빈 후처리기 같은 느낌.. 실제 Program 구동시 등록되는 DbContext 관련 의존성을 제거하고, 메모리 디비로 변경
+    public class CustomWebApplicationFactory<TProgram>
+        : WebApplicationFactory<TProgram> where TProgram : class
+    {
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<ShipParticularsContext>));
+
+                services.Remove(dbContextDescriptor);
+
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbConnection));
+
+                services.Remove(dbConnectionDescriptor);
+
+                // Create open SqliteConnection so EF won't automatically close it.
+                services.AddSingleton<DbConnection>(container =>
+                {
+                    var connection = new SqliteConnection("DataSource=:memory:");
+                    connection.Open();
+                    return connection;
+                });
+
+                services.AddDbContext<ShipParticularsContext>((container, options) =>
+                {
+                    var connection = container.GetRequiredService<DbConnection>();
+                    options.UseSqlite(connection);
+                    options.UseLazyLoadingProxies();
+                    options.EnableSensitiveDataLogging();
+                });
+
+            });
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
