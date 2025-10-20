708b648ed33a35ffb57d92bd7282d77fd06d6555
leejinwoo <julee@vessellink.com>
2025-10-20 16:55:41 +0900

    서비스 인터페이스에 조회 시그니처 정의, 구현

--- ShipParticularsApi/Services/IShipParticularsService.cs
+++ ShipParticularsApi/Services/IShipParticularsService.cs
@@ -1,9 +1,11 @@
-﻿using ShipParticularsApi.Services.Dtos;
+﻿using ShipParticularsApi.Services.Dtos.Params;
+using ShipParticularsApi.Services.Dtos.Results;
 
 namespace ShipParticularsApi.Services
 {
     public interface IShipParticularsService
     {
         Task Process(ShipParticularsParam param);
+        Task<ShipParticularsResult> GetShipParticulars(string shipKey);
     }
 }
