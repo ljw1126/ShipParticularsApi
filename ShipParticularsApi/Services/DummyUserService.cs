ab90f4120a24627484299f576ceda2b0a663a87a
이진우 <leejinwoo1126@gmail.com>
2025-10-21 13:14:56 +0900

    고정된 userId값 가져오기 위해 IUserService 선언, 구현체 생성

---
+++ ShipParticularsApi/Services/DummyUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace ShipParticularsApi.Services
+{
+    public class DummyUserService : IUserService
+    {
+        public string GetCurrentUserId()
+        {
+            return "TEST_USER_01";
+        }
+    }
+}
