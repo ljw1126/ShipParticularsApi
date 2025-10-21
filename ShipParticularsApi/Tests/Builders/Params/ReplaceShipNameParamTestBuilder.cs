6f0c02deedbb0ebe4b1b94d7169e297f55baf6e1
이진우 <leejinwoo1126@gmail.com>
2025-10-21 18:40:34 +0900

    param test data builder 생성

---
+++ ShipParticularsApi/Tests/Builders/Params/ReplaceShipNameParamTestBuilder.cs
@@ -0,0 +1,28 @@
+﻿using ShipParticularsApi.Services.Dtos.Params;
+
+namespace ShipParticularsApi.Tests.Builders.Params
+{
+    public class ReplaceShipNameParamTestBuilder
+    {
+        private string _ReplacedShipName;
+
+        public static ReplaceShipNameParamTestBuilder ReplaceShipNameParam()
+        {
+            return new ReplaceShipNameParamTestBuilder();
+        }
+
+        public ReplaceShipNameParamTestBuilder WithReplacedShipName(string replacedShipName)
+        {
+            this._ReplacedShipName = replacedShipName;
+            return this;
+        }
+
+        public ReplaceShipNameParam Build()
+        {
+            return new()
+            {
+                ReplacedShipName = _ReplacedShipName
+            };
+        }
+    }
+}
