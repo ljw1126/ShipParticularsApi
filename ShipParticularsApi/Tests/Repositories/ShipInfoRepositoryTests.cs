9646836f8b0f35a888fe8391b84867fd2ca91197
이진우 <leejinwoo1126@gmail.com>
2025-10-22 10:40:30 +0900

    Repository 테스트 중복 제거, 픽스처 사용

--- ShipParticularsApi/Tests/Repositories/ShipInfoRepositoryTests.cs
+++ ShipParticularsApi/Tests/Repositories/ShipInfoRepositoryTests.cs
@@ -2,14 +2,11 @@
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using ShipParticularsApi.Contexts;
-using ShipParticularsApi.Entities.Enums;
 using ShipParticularsApi.Repositories;
 using Xunit;
 using Xunit.Abstractions;
 
 using static ShipParticularsApi.Tests.Builders.Entities.ShipInfoTestBuilder;
-using static ShipParticularsApi.Tests.Builders.Entities.ShipSatelliteTestBuilder;
-using static ShipParticularsApi.Tests.Builders.Entities.ShipServiceTestBuilder;
 
 
 namespace ShipParticularsApi.Tests.Repositories
@@ -49,70 +46,52 @@ namespace ShipParticularsApi.Tests.Repositories
         public async Task AsTracking()
         {
             // Arrange
-            const string shipKey = "SHIP_KEY";
-            var newShipInfo = ShipInfo()
-                   .WithShipKey(shipKey)
-                   .WithCallsign("TEST_CALLSIGN")
-                   .WithShipName("TEST_SHIP_NAMME")
-                   .WithShipType(ShipTypes.Fishing)
-                   .WithShipCode("TEST_SHIP_CODE")
-                   .WithShipServices(KtSatService(shipKey))
-                   .WithShipSatellite(KtSatellite(shipKey, "SATELLITE_ID"))
-                   .WithExternalShipId("SATELLITE_ID")
-                   .WithIsUseKtsat(true)
-                   .Build();
+            const string shipKey = "UNIQUE_SHIP_KEY";
 
             await using (var arrangeContext = CreateContext())
             {
-                arrangeContext.ShipInfos.Add(newShipInfo);
+                arrangeContext.ShipInfos.Add(NoService(shipKey, 1L).Build());
                 await arrangeContext.SaveChangesAsync();
             }
 
-            var dbContext = CreateContext();
-            var repository = new ShipInfoRepository(dbContext);
+            // Act & Assert
+            await using (var dbContext = CreateContext())
+            {
+                var repository = new ShipInfoRepository(dbContext);
 
-            // Act
-            var savedShipInfo = await repository.GetByShipKeyAsync(shipKey);
+                var actual = await repository.GetByShipKeyAsync(shipKey);
 
-            // Assert
-            savedShipInfo.Should().NotBeNull();
-            var entry = dbContext.Entry(savedShipInfo!);
-            entry.State.Should().Be(EntityState.Unchanged);
+                actual.Should().NotBeNull();
+
+                var entry = dbContext.Entry(actual!);
+                entry.State.Should().Be(EntityState.Unchanged);
+            }
         }
 
         [Fact(DisplayName = "AsNoTracking()으로 조회한 엔티티는 상태추적하지 않는다.")]
         public async Task AsNoTracking()
         {
             // Arrange
-            const string shipKey = "SHIP_KEY";
-            var newShipInfo = ShipInfo()
-                   .WithShipKey(shipKey)
-                   .WithCallsign("TEST_CALLSIGN")
-                   .WithShipName("TEST_SHIP_NAMME")
-                   .WithShipType(ShipTypes.Fishing)
-                   .WithShipCode("TEST_SHIP_CODE")
-                   .WithShipServices(KtSatService(shipKey))
-                   .WithShipSatellite(KtSatellite(shipKey, "SATELLITE_ID"))
-                   .WithExternalShipId("SATELLITE_ID")
-                   .WithIsUseKtsat(true)
-                   .Build();
+            const string shipKey = "UNIQUE_SHIP_KEY";
 
             await using (var arrangeContext = CreateContext())
             {
-                arrangeContext.ShipInfos.Add(newShipInfo);
+                arrangeContext.ShipInfos.Add(NoService(shipKey, 1L).Build());
                 await arrangeContext.SaveChangesAsync();
             }
 
-            var dbContext = CreateContext();
-            var repository = new ShipInfoRepository(dbContext);
+            // Act & Assert
+            await using (var dbContext = CreateContext())
+            {
+                var repository = new ShipInfoRepository(dbContext);
 
-            // Act
-            var savedShipInfo = await repository.GetReadOnlyByShipKeyAsync(shipKey);
+                var actual = await repository.GetReadOnlyByShipKeyAsync(shipKey);
 
-            // Assert
-            savedShipInfo.Should().NotBeNull();
-            var entry = dbContext.Entry(savedShipInfo!);
-            entry.State.Should().Be(EntityState.Detached);
+                actual.Should().NotBeNull();
+
+                var entry = dbContext.Entry(actual!);
+                entry.State.Should().Be(EntityState.Detached);
+            }
         }
     }
 }
