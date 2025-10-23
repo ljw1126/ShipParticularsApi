dac9d5db4197d227c1dcdf02bbfb3aa1a05d6826
leejinwoo <julee@vessellink.com>
2025-10-23 13:31:50 +0900

    temp

---
+++ ShipParticularsApi/Extensions/GlobalExceptionMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ShipParticularsApi.Middlewares;
+
+namespace ShipParticularsApi.Extensions
+{
+    public static class GlobalExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGlobalExceptioHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<GlobalExceptionMiddleware>();
+        }
+    }
+}
