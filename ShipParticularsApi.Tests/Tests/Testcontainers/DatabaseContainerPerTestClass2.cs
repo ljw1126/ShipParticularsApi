09a3a7a85fba01894d4bba26176f1817d332eee9
이진우 <leejinwoo1126@gmail.com>
2025-11-04 14:18:05 +0900

    Container-per-class 전략이 실제로 하나의 컨테이너로 클래스에서 공유하는지 확인

---
+++ ShipParticularsApi.Tests/Tests/Testcontainers/DatabaseContainerPerTestClass2.cs
@@ -0,0 +1,20 @@
+﻿using Xunit.Abstractions;
+
+namespace ShipParticularsApi.Tests.Tests.Testcontainers
+{
+    public class DatabaseContainerPerTestClass2(DatabaseFixture fixture, ITestOutputHelper output)
+        : IClassFixture<DatabaseFixture>, IDisposable
+    {
+        public void Dispose()
+        {
+            output.WriteLine($"Container Id = {fixture.ContainerId}");
+        }
+
+        [Fact]
+        public void SampleTest()
+        {
+            // Sample test implementation
+            output.WriteLine("This is a sample test.");
+        }
+    }
+}
